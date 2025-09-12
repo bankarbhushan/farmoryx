03b100d48582ba7d77d63e2650e616d6f6bad424
bhushan <bhushan.bankar@suavisinc.com>
2025-09-12 14:29:45 +0530

    Dashboard ui correct

---
+++ client/src/components/constants/Card.jsx
@@ -0,0 +1,20 @@
+// src/constants/Card.jsx
+import React from "react";
+
+const Card = ({ title, value, icon, className }) => {
+  return (
+    <div className={`bg-white border border-gray-200 rounded-lg shadow-sm p-5 flex items-sat gap-4`}>
+      
+      {/* {icon && <div className={`text-3xl text-green-600 ${className}}`>{icon}</div>} */}
+      {
+        icon && <div className={`text-3xl ${className}`}>{icon} </div>
+      }
+      <div>
+        <h3 className="text-sm text-gray-500 font-medium ">{title}</h3>
+        <p className="text-xl font-bold text-gray-800">{value}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
